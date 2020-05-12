9bbdc044e0dba32ce890cd19b5e77a31e31d5520
suiyebuluo <guoju58@163.com>
2020-05-12 20:34:42 +0800

    221最大正方形

--- top100/shuzu_max_zhengfangxing.java
+++ top100/shuzu_max_zhengfangxing.java
@@ -8,13 +8,14 @@ public class shuzu_max_zhengfangxing {
         int[][] dp=new int[rows][cols];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                if (matrix[i][j]=='1'){
-                    dp[i][j]=1;
+                if (matrix[i][j]=='1') {
+                    if (i == 0 || j == 0)
+                        dp[i][j] = 1;
+                    else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
                 }
-                else{
-                    dp[i][j]=Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
-                }
-                maxSide=Math.max(maxSide,dp[i][j]);
             }
         }
         int maxSquare=maxSide*maxSide;
