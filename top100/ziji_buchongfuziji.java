323615e89f82036cab16cdf2070be680e35c4eae
suiyebuluo <guoju58@163.com>
2020-05-19 14:45:11 +0800

    78子集

--- top100/ziji_buchongfuziji.java
+++ top100/ziji_buchongfuziji.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * 78子集
+ * 给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+ * 说明：解集不能包含重复的子集。
+ */
 public class ziji_buchongfuziji {
 //    public static List<List<Integer>> subsets(int[] nums){
 //        List<List<Integer>> res=new ArrayList<>();
