f6e333a4b2b9ad1bf37c21957b1ae23a2f451536
suiyebuluo <guoju58@163.com>
2020-05-19 16:16:46 +0800

    79单词搜索

--- top100/dancisousuo.java
+++ top100/dancisousuo.java
@@ -1,5 +1,9 @@
     public class dancisousuo {
-
+        /**
+         * 79单词搜索
+         * 给定一个二维网格和一个单词，找出该单词是否存在于网格中。
+         * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+         */
         private boolean[][] marked;
 
         //        x-1,y
@@ -59,17 +63,14 @@
 
         public static void main(String[] args) {
 
-        char[][] board =
-                {
+            char[][] board =
+                    {
                         {'A', 'B', 'C', 'E'},
                         {'S', 'F', 'C', 'S'},
                         {'A', 'D', 'E', 'E'}
-                };
-
-        String word = "ABCCED";
+                    };
+            String word = "ABCCED";
 //            String word = "ABCD";
-
-
 //            char[][] board = {{'a', 'b'}};
 //            String word = "ba";
             dancisousuo solution = new dancisousuo();
