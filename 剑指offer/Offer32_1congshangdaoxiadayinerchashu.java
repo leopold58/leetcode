a9760833ba1ae14368d11e18d241add198ffd0f5
suiyebuluo <guoju58@163.com>
2020-07-18 00:23:10 +0800

    32-I.从上到下打印二叉树

---
+++ 剑指offer/Offer32_1congshangdaoxiadayinerchashu.java
@@ -0,0 +1,36 @@
+package SwordOffer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 32 - I. 从上到下打印二叉树
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+ */
+public class Offer32_1congshangdaoxiadayinerchashu {
+    public class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x){val=x;}
+    }
+    public int[] levelOrder(TreeNode root){
+        if (root==null) return new int[0];
+        ArrayList<Integer> list = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left!=null) queue.add(node.left);
+            if (node.right!=null) queue.add(node.right);
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i]=list.get(i);
+        }
+        return res;
+    }
+}
